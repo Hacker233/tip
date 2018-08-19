2704cf6a7123468f1182a833a77b32288bbc997f
lu.hongquan <1584775297@qq.com>
2018-08-19 22:11:10 +0800

    移动端适配

---
+++ src/components/Home/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <home></home>
+    </div>
+</template>
+<script>
+import home from '../luHeader.vue'
+export default {
+  data () {
+
+  },
+  components: {
+    home
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/luHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <p>用心创作</p>
+    </div>
+</template>
+<script>
+export default {
+  data () {}
+}
+</script>
+
+<style>
+.wrapper {
+    width: 750px;
+    height: 100px;
+    background-color: rgba(61, 89, 180,0.5);
+    text-align: center;
+}
+.wrapper p {
+    line-height: 100px;
+    letter-spacing: 20px;
+    font-size: 30px;
+}
+</style>
